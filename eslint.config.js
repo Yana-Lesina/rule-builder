1dee498c764eec43bfdcd2083e8a0056b2a0bd3e
Yana-Lesina <yanalesina3@gmail.com>
2025-06-04 02:49:41 +0300

    update configs

--- eslint.config.js
+++ eslint.config.js
@@ -64,6 +64,8 @@ export default tseslint.config(
       'react/button-has-type': 'error',
       'react/react-in-jsx-scope': ['off'],
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      'react/prop-types': 0,
     },
   },
   {
